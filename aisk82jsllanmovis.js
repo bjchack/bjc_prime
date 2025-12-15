1eeaa24b9319c6bf190e0788b48f960f35939924
bjchack <bjchack@gmail.com>
2025-12-15 19:45:02 +0800

    Create aisk82jsllanmovis.js

---
+++ aisk82jsllanmovis.js
@@ -0,0 +1,21 @@
+// MOVIE SOURCE (JS ONLY)
+window.MOVIES = [
+    {
+        title: "Avengers Infinity War",
+        img: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_FMjpg_UX1000_.jpg",
+        file: "http://188.210.209.2/EDI/Movies/Avengers.Infinity.War.2018.1080p.BluRay.x264-[YTS.AM].mp4",
+        cat: "trending, action"
+    },
+    {
+        title: "Barbie 2023",
+        img: "https://a.ltrbxd.com/resized/film-poster/2/7/7/0/6/4/277064-barbie-0-230-0-345-crop.jpg",
+        file: "http://188.210.209.2/EDI/Movies/Barbie.2023.1080p.WEBRip.x264.AAC5.1-[YTS.MX].mp4",
+        cat: "romance"
+    },
+    {
+        title: "Mermaids 1 (Tagalog)",
+        img: "https://marvel-b1-cdn.bc0a.com/f00000000280066/d2snwnmzyr8jue.cloudfront.net/nvg_nnvg10007161_270.jpeg",
+        file: "http://188.210.209.2/EDI/Movies/Discovery.Ch.Mermaids.1of2.The.Body.Found.PDTV.x264.AAC.MVGroup.org.mp4",
+        cat: "romance"
+    }
+];
