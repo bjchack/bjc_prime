0d7cc36e8a7b8ab7cc26ac6ec2e064a629e51919
bjchack <bjchack@gmail.com>
2025-12-16 00:03:40 +0800

    Update aisk82jsllanmovis.js

--- aisk82jsllanmovis.js
+++ aisk82jsllanmovis.js
@@ -10,12 +10,42 @@ window.MOVIES = [
         title: "Barbie 2023",
         img: "https://a.ltrbxd.com/resized/film-poster/2/7/7/0/6/4/277064-barbie-0-230-0-345-crop.jpg",
         file: "http://188.210.209.2/EDI/Movies/Barbie.2023.1080p.WEBRip.x264.AAC5.1-[YTS.MX].mp4",
-        cat: "romance"
+        cat: "romantic"
     },
     {
         title: "Mermaids 1 (Tagalog)",
         img: "https://marvel-b1-cdn.bc0a.com/f00000000280066/d2snwnmzyr8jue.cloudfront.net/nvg_nnvg10007161_270.jpeg",
         file: "http://188.210.209.2/EDI/Movies/Discovery.Ch.Mermaids.1of2.The.Body.Found.PDTV.x264.AAC.MVGroup.org.mp4",
-        cat: "romance"
+        cat: "romantic"
+    },
+    {
+        title: "Guardians of the Galaxy",
+        img: "https://upload.wikimedia.org/wikipedia/en/0/08/Guardians_of_the_Galaxy_vol_2_poster.jpg",
+        file: "http://188.210.209.2/EDI/Movies/Guardians.of.the.Galaxy.Vol.2.2017.1080p.BluRay.x264-[YTS.AM].mp4",
+        cat: "trending, action, scifi"
+    },
+    {
+        title: "The Batman 2022",
+        img: "https://upload.wikimedia.org/wikipedia/en/1/17/The_Batman_Theatrical_Poster.jpg",
+        file: "http://188.210.209.2/EDI/Movies/The.Batman.2022.1080p.BluRay.x264-[YTS.AM].mp4",
+        cat: "trending, action"
+    },
+    {
+        title: "Spider-Man: No Way Home",
+        img: "https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_No_Way_Home_poster.jpg",
+        file: "http://188.210.209.2/EDI/Movies/Spider.Man.No.Way.Home.2021.1080p.BluRay.x264-[YTS.AM].mp4",
+        cat: "action, scifi"
+    },
+    {
+        title: "The Notebook",
+        img: "https://upload.wikimedia.org/wikipedia/en/8/88/The_Notebook_cover.jpg",
+        file: "http://188.210.209.2/EDI/Movies/The.Notebook.2004.1080p.BluRay.x264-[YTS.MX].mp4",
+        cat: "romantic"
+    },
+    {
+        title: "Jurassic World",
+        img: "https://upload.wikimedia.org/wikipedia/en/e/ed/Jurassic_World_poster.jpg",
+        file: "http://188.210.209.2/EDI/Movies/Jurassic.World.2015.1080p.BluRay.x264-[YTS.AM].mp4",
+        cat: "action, scifi"
     }
 ];
